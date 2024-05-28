f3bc0313ed4a0707b0885e4c8a17266fcb822620
John-Rashta <john_rashta@outlook.com>
2024-05-28 16:26:14 +0100

    Finish exercise 3
    commiting exercise 3, forgot split returns an array, used
    arrayfrom instead

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    let stringArray = Array.from(string);
+
+    stringArray.reverse();
+
+    return stringArray.join("");
+    
 };
 
 // Do not edit below this line
