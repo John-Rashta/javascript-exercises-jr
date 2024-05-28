4f80fb1712dcb81baa7c4b557c1d2b1758fd3fa9
John-Rashta <john_rashta@outlook.com>
2024-05-28 18:25:26 +0100

    Finish exercise 6
    Very simple solution from me, could be done better,
    forgot i could use OR to check both 100 and 400 cases at
    the same time since the are mutually exclusive

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,20 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    if (!(year % 4)) {
+
+        if (!(year % 100)) {
+            if (!(year % 400)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+
+    return false;
 
 };
 
