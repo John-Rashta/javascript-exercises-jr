c73fd550accee10499092da7a73b5c75ae45d905
John-Rashta <john_rashta@outlook.com>
2024-05-28 17:50:36 +0100

    Finish exercise 4
    Honestly really suboptimal, went with 2 loops instead of the
    easiest and more effective ways of for each and filter, honestly main
    reason for that is that i thought the include method didnt properly compare,
    will have my solution as the loops tho, just gotta keep in mind things like the
    more effective solution to improve how i code

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(originalArray, ...toRemoveItems) {
+
+    const removeLength = toRemoveItems.length;
+
+    let i = originalArray.length - 1;
+    while (i >= 0 ) {
+        for (let j = 0; j < removeLength; j++ ) {
+
+            if (originalArray[i] === toRemoveItems[j]) {
+                originalArray.splice(i, 1);
+
+            }
+        }
+        
+        i--;
+
+    }
+
+    return originalArray;
 };
 
 // Do not edit below this line
