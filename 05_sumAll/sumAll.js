c5759fe44299ffee6b211ae0b8d1b0c154781618
John-Rashta <john_rashta@outlook.com>
2024-05-28 18:12:56 +0100

    Finish exercise 5
    committing exercise 5, could be done a bit better with less variables
    but wasnt as bad as previous one, need to remember i dont need brackets
    for if statements that have just 1 return

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,32 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, secondNumber) {
+
+    if (!Number.isInteger(firstNumber) || !Number.isInteger(secondNumber)) return "ERROR";
+    
+
+    if (firstNumber < 0 || secondNumber < 0) return "ERROR";
+    
+
+    let startNumber;
+    let endNumber;
+
+    if (firstNumber > secondNumber) {
+
+        startNumber = secondNumber;
+        endNumber = firstNumber;
+        
+    } else {
+
+        startNumber = firstNumber;
+        endNumber = secondNumber;
+    }
+
+    total = 0;
+
+    for (let i = startNumber; i <= endNumber; i++) {
+        total += i;
+    }
+
+    return total;
 
 };
 
