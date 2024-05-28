8bff2c7f59438f58dbeba2ea6d5deb237d921a38
John-Rashta <john_rashta@outlook.com>
2024-05-28 18:44:43 +0100

    Finish exercise 7
    Commit exercise 7, a bit diferent from solution but i think
    it wasnt worse, at least not much worse so not unhappy about this one

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahren) {
+
+  let celsius = (fahren - 32) * (5 / 9);
+
+  return Number(celsius.toFixed(1));
+
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+
+  let fahren = celsius * (9 / 5) + 32;
+
+  return Number(fahren.toFixed(1));
 };
 
 // Do not edit below this line
