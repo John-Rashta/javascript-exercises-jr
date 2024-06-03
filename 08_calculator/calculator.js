62b607cc7e4e4e0073882ce4bc30646a062579c1
John-Rashta <john_rashta@outlook.com>
2024-06-03 22:41:45 +0100

    Finish exercise 8
    commiting exercise 8, pretty similar to solution so not bad

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,46 @@
-const add = function() {
+const add = function(a, b) {
+
+  return a + b;
 	
 };
 
-const subtract = function() {
+const subtract = function(a, b) {
+  return a - b;
 	
 };
 
-const sum = function() {
+const sum = function(...nums) {
+
+
+  return nums.flat().reduce((total, num) => total + num, 0);
 	
 };
 
-const multiply = function() {
+const multiply = function(...nums) {
+
+  return nums.flat().reduce((total, num) => total * num);
 
 };
 
-const power = function() {
+const power = function(num, power) {
+
+  return num ** power;
 	
 };
 
-const factorial = function() {
+const factorial = function(num) {
+
+  if (num === 0) {
+    return 1;
+  }
+
+  let fact = 1;
+
+  for (i = 1; i <= num; i++) {
+    fact = fact * i;
+  }
+
+  return fact;
 	
 };
 
