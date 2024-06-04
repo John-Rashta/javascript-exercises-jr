908d2873d0eb8e5826e1e574cfd5886d08b94341
John-Rashta <john_rashta@outlook.com>
2024-06-04 16:26:42 +0100

    Finish Exercise 12
    Committing exercise 12, decent solution i think, was stuck a bit
    cause i forgot i needed to return something so on second loop accumulator
    always became undefined and i was confused how cause everything looked fine
    and it was, only problem was i wasnt returning the value, made a small change
    after seeing the solution, changed the if statement for return into a ternary aswell,
    forgot about it but it does look cleaner like that, need to remember more to use that
    kind of operation since it makes writing this if statements easier

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) { 
 
+    return people.reduce((oldest, person) => {
+
+      
+        if (!Object.hasOwn(person, "yearOfDeath")) {
+            person.yearOfDeath = new Date().getFullYear();
+        }
+
+        if (!Object.hasOwn(oldest, "yearOfDeath")) {
+            oldest.yearOfDeath = new Date().getFullYear();
+        }
+
+        let newAge = person.yearOfDeath - person.yearOfBirth;
+        let oldAge = oldest.yearOfDeath - oldest.yearOfBirth;
+
+        return oldAge > newAge ? oldest : person;
+
+    })
 };
 
 // Do not edit below this line
