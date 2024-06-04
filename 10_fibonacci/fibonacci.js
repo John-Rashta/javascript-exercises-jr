eeca81cc96b93dee920ad29a1a02604fb6e412a2
John-Rashta <john_rashta@outlook.com>
2024-06-04 15:27:28 +0100

    Finish exercise 10
    Commiting exercise 10, kinda diferent from solution but
    the idea is pretty similar just diferent ways to get around it

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,37 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+
+    newNum = Number(num);
+
+    if (newNum === 0) {
+        
+        return 0;
+    }
+
+    if (newNum < 0) {
+        
+        return "OOPS";
+    }
+
+    let firstNum = 1;
+    let secondNum = 0;
+
+    for (let i = 1; i < newNum; i++) {
+
+        if (i % 2 === 0) {
+
+            secondNum += firstNum;
+
+        } else {
+
+            firstNum += secondNum;
+
+
+        }
+
+
+    }
+
+    return firstNum + secondNum;
 
 };
 
